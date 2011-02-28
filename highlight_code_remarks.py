0b12050f33144aa44d0d6bfc2e4725b59b503319
Oktay Acikalin <oktay.acikalin@gmail.com>
2011-02-28 09:38:44 +0100

    Optimized regex. Added max file size check to prevent dossing the editor.

--- highlight_code_remarks.py
+++ highlight_code_remarks.py
@@ -2,6 +2,10 @@
 Event listener which scans content, finds special remark words like FIXME,
 TODO, WARNING, INFO or DONE and highlights them.
 
+You might want to override the following parameters within your file settings:
+* highlight_code_remarks_max_file_size
+  Restrict this to a sane size in order not to DDOS your editor.
+
 Add these to your theme (and optionally adapt the colors to your liking):
         <dict>
             <key>name</key>
@@ -114,6 +118,8 @@ import sublime
 import sublime_plugin
 
 
+DEFAULT_MAX_FILE_SIZE = 1048576
+
 REMARK_COLORS = {
     'TODO': 'remark.todo',
     'DONE': 'remark.done',
@@ -133,9 +139,9 @@ class HighlightCodeRemarksListener(sublime_plugin.EventListener):
         color_keys, color_values = zip(*REMARK_COLORS.items())
         # print color_keys
         # print color_values
-        pattern = r'(?:^[*]+\s*)?\<%s\>(?:[^\'\"\n\[\]]+?[:])?'
-        regex_colors = [pattern % color_key for color_key in color_keys]
-        regex = '(?:' + ')|(?:'.join(regex_colors) + ')'
+        pattern = r'(?:^[*]+\s*)?\<(%s)\>(?:[^\'\"\n\[\]]+?[:])?'
+        regex = pattern % '|'.join(color_keys)
+        regex = '(?:' + regex + ')'
         # print 'regex =', regex
         self.regex = regex
         self.color_keys = color_keys
@@ -174,8 +180,22 @@ class HighlightCodeRemarksListener(sublime_plugin.EventListener):
                 view.erase_regions(tag)
 
     def defered_update(self, view):
-        if bool(view.settings().get('is_widget')):
+        settings = view.settings()
+        if bool(settings.get('is_widget')):
             return
+        
+        max_size = settings.get('highlight_code_remarks_max_file_size',
+                                DEFAULT_MAX_FILE_SIZE)
+        # print max_size, type(max_size)
+        if max_size not in (None, False):
+            max_size = long(max_size)
+            cur_size = view.size()
+            if cur_size > max_size:
+                for color_value in self.color_values:
+                    tag = 'HighlightCodeRemarksListener.%s' % color_value
+                    view.erase_regions(tag)
+                return
+        
         def func():
             self.update(view)
         sublime.set_timeout(func, 500)
